99cd6c96912312cff993f3067e72230b107a6887
pablord14 <pablo.rd14@gmail.com>
2017-07-11 19:11:35 -0300

    Agregada llamada a servicios

--- app/src/main/java/com/mobile/dacs/borschimobile/LoginActivity.java
+++ app/src/main/java/com/mobile/dacs/borschimobile/LoginActivity.java
@@ -1,12 +1,23 @@
 package com.mobile.dacs.borschimobile;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.mobile.dacs.borschimobile.model.Cliente;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -15,17 +26,15 @@ public class LoginActivity extends AppCompatActivity {
     String passwordtxt;
     EditText password;
     EditText username;
-
+    Cliente cliente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
         username = (EditText) findViewById(R.id.username);
         password = (EditText) findViewById(R.id.password);
         loginbutton = (Button) findViewById(R.id.login);
-
         loginbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -35,25 +44,68 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void onLoginButtonClicked(){
-        // Retrieve the text entered from the EditText
         usernametxt = username.getText().toString();
         passwordtxt = password.getText().toString();
 
-        // Send data to WS for verification
-        if (true) {
-            // If user exist and authenticated, send user to Welcome.class
+            AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                URL endpoint = null;
+                Gson gson = new GsonBuilder().create();
+                HttpURLConnection myConnection = null;
+                try {
+//                    endpoint = new URL("https://api.github.com/users/mralexgray/repos");
+
+//                    usernametxt = "pablord14";
+                    usernametxt = "alanfiz92";
+//                    passwordtxt = "rdpjgs07";
+                    passwordtxt = "34gw4ef34";
+//                    endpoint = new URL("http://192.168.0.104:8080/dacstpi/api/login?user="+usernametxt+"&pass="+passwordtxt);
+                    endpoint = new URL("http://192.168.0.105:8080/greeting");
+//                    endpoint = new URL("http://192.168.0.104:8080/dacstpi/api/login?user=alanfiz92&pass=34gw4ef34");
+//                    endpoint = new URL("http://192.168.43.252:8080/dacstpi/api/tarjeta");
+                    myConnection = (HttpURLConnection) endpoint.openConnection();
+
+                    if (myConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                        InputStream responseBody = myConnection.getInputStream();
+                        InputStreamReader responseBodyReader =
+                                new InputStreamReader(responseBody, "UTF-8");
+                        cliente = gson.fromJson(responseBodyReader, Cliente.class);
+                    } else {
+                        cliente = null;
+                    }
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    myConnection.disconnect();
+                }
+            }
+        });
+
+        try {
+            TimeUnit.SECONDS.sleep(Long.valueOf("4"));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        if (cliente != null) {
             Intent intent = new Intent(
                     LoginActivity.this,
                     UserActivity.class);
+            intent.putExtra("cliente", cliente);
             startActivity(intent);
             Toast.makeText(getApplicationContext(),
-                    "Successfully Logged in",
+                    "Logeo Exitoso",
                     Toast.LENGTH_LONG).show();
             finish();
         } else {
             Toast.makeText(
                     getApplicationContext(),
-                    "No such user exist, please signup",
+                    "Usuario inexistente",
                     Toast.LENGTH_LONG).show();
         }
     }
--- app/src/main/java/com/mobile/dacs/borschimobile/UserActivity.java
+++ app/src/main/java/com/mobile/dacs/borschimobile/UserActivity.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.JsonReader;
@@ -17,34 +19,46 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.mobile.dacs.borschimobile.model.Cliente;
+import com.mobile.dacs.borschimobile.model.Producto;
+import com.mobile.dacs.borschimobile.model.Productos;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import javax.net.ssl.HttpsURLConnection;
 
 public class UserActivity extends AppCompatActivity {
 
+    private static final String ESPACIO = "      ";
     Button buscarButton;
     EditText buscarEditText;
     ListView buscarLista;
     final Context context = this;
-    List productosLista = new ArrayList();
+    Productos productos;
+    Cliente cliente;
+    List <String> productosLista = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
 
+        cliente = (Cliente) getIntent().getSerializableExtra("cliente");
         buscarEditText = (EditText) findViewById(R.id.buscarEditText);
         buscarButton = (Button) findViewById(R.id.buscarButton);
         buscarLista = (ListView) findViewById(R.id.productosLista);
-
         buscarButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -54,59 +68,52 @@ public class UserActivity extends AppCompatActivity {
     }
 
     private void buscarButtonClicked() {
-        String buscarString = buscarEditText.getText().toString();
-//        AsyncTask.execute(new Runnable() {
-//            @Override
-//            public void run() {
-//                // Create URL
-//                URL endpoint = null;
-//                try {
-//                    endpoint = new URL("http://192.168.1.50:8080/dacstpi-g3/api/tarjeta");
-//                    HttpURLConnection myConnection =
-//                            (HttpURLConnection) endpoint.openConnection();
-//
-//                    if (myConnection.getResponseCode() == 200) {
-//                        InputStream responseBody = myConnection.getInputStream();
-//                        InputStreamReader responseBodyReader =
-//                                new InputStreamReader(responseBody, "UTF-8");
-//                        JsonReader jsonReader = new JsonReader(responseBodyReader);
-//                        jsonReader.beginArray();
-//                        jsonReader.beginObject();
-//                        while (jsonReader.hasNext()) {
-//                            String key = jsonReader.toString();
-//                            System.out.print(key);
-//                        }
-//                    } else {
-//                        // Error handling code goes here
-//                    }
-//                    myConnection.disconnect();
-//                } catch (MalformedURLException e) {
-//                    e.printStackTrace();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                } catch (Exception e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        });
-        String[] FRUITS = new String[] { "Apple", "Avocado", "Banana",
-                "Blueberry", "Coconut", "Durian", "Guava", "Kiwifruit",
-                "Jackfruit", "Mango", "Olive", "Pear", "Sugar-apple" };
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                String stringBusqueda = buscarEditText.getText().toString();
+                URL endpoint = null;
+                Gson gson = new GsonBuilder().create();
+                try {
+//                    endpoint = new URL("http://192.168.0.104:8080/dacstpi/api/productos?query="+stringBusqueda);
+                    endpoint = new URL("http://192.168.0.105:8080/timeGreeting");
+                    HttpURLConnection myConnection = (HttpURLConnection) endpoint.openConnection();
+                    if (myConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                        InputStream responseBody = myConnection.getInputStream();
+                        InputStreamReader responseBodyReader = new InputStreamReader(responseBody, "UTF-8");
+                        productos = gson.fromJson(responseBodyReader, Productos.class);
+                    } else {
+                        // Error handling code goes here
+                    }
+                    myConnection.disconnect();
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        try {
+            TimeUnit.SECONDS.sleep(Long.valueOf("5"));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        for (Producto producto: productos.getProductos())  {
+            productosLista.add(producto.getNombreProducto().substring(0, 25).concat(ESPACIO)
+                    .concat(producto.getPrecioUnitarioProducto().toString()));
+        }
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, android.R.id.text1, FRUITS);
+                android.R.layout.simple_list_item_1, android.R.id.text1, productosLista);
         buscarLista.setAdapter(adapter);
         buscarLista.setTextFilterEnabled(true);
         buscarLista.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-
             public boolean onItemLongClick(AdapterView<?> parent, final View view,
                                            int position, long id) {
-//                Toast.makeText(getApplicationContext(),
-//                        ((TextView) view).getText(), Toast.LENGTH_SHORT).show();
-//                Intent intent = new Intent(
-//                        UserActivity.this,
-//                        PopUpCarritoActivity.class);
-//                startActivity(intent);
                 AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
                         context);
                 alertDialogBuilder.setTitle("Comprar..");
@@ -115,7 +122,7 @@ public class UserActivity extends AppCompatActivity {
                         .setCancelable(false)
                         .setPositiveButton("Yes",new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog,int id) {
-                                productosLista.add(((TextView) view).getText());
+////                                listaCarrito.add(((TextView) view).getId());
                                 dialog.cancel();
                             }
                         })
